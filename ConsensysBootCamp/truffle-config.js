04aa41b67f5eb8c1b1f13aca95a56ab55329afc2
Graeme <graemebarnes1987@gmail.com>
2021-01-04 10:59:15 +0100

    Update truffle-config.js

--- ConsensysBootCamp/truffle-config.js
+++ ConsensysBootCamp/truffle-config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     rinkeby: {
       provider: function() { 
-       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/59646ee14226404090d8f55b51f6abd7");
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/YOUR_API_KEY");
       },
       network_id: 4,
       gas: 4500000,
